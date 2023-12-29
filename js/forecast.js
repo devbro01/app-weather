ecde74c70603e18b3c5092a9396b3a1e0103c588
devbro01 <mygame75484@gmail.com>
2023-12-30 01:19:08 +0500

    doing updates but icon is not ready

--- js/forecast.js
+++ js/forecast.js
@@ -1,2 +1,11 @@
-const KEY = '96b947a45d33d7dc1c49af3203966408'
+const KEY = "96b947a45d33d7dc1c49af3203966408";
 
+// requist for get data
+const getData = async (city) => {
+  const base = "https://api.openweathermap.org/data/2.5/weather";
+  const query = `?q=${city}&units=metric&appid=${KEY}`;
+
+  const req = await fetch(base + query);
+  const data = await req.json();
+  return data;
+};
