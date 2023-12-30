99447368765363b5f26321941fb454dcadde05cc
devbro01 <mygame75484@gmail.com>
2023-12-31 00:57:25 +0500

    all done weather-app

--- js/app.js
+++ js/app.js
@@ -4,24 +4,36 @@ const details = document.getElementById("details");
 const weatherIcon = document.getElementById("weather-icon");
 const overlay = document.getElementById("overlay");
 
-changeLocation.city.focus()
+changeLocation.city.focus();
 
+// for loader
+
+function loader(loading) {
+  if (loading) {
+    overlay.classList.remove("d-none");
+  } else {
+    overlay.classList.add("d-none");
+  }
+}
 
 // updateUI
 const updateUI = (weather) => {
-  console.log(weather);
   details.innerHTML = `
-    <h5 class="mb-3">${weather.name} ${weather.sys.country}</h5>
-    <p class="mb-3">${weather.weather[0].main}</p>
-    <div class="display-4 mb-3">
-      <span>${Math.round(weather.main.temp)}</span>
-      <span>&deg;C</span>
-    </div>
-    `;
+  <h5 class="mb-3">${weather.name} ${weather.sys.country}</h5>
+  <p class="mb-3">${weather.weather[0].main}</p>
+  <div class="display-4 mb-3">
+  <span>${Math.round(weather.main.temp)}</span>
+  <span>&deg;C</span>
+  </div>
+  `;
 
   if (card.classList.contains("d-none")) {
     card.classList.remove("d-none");
   }
+
+  // for weather icon
+
+  weatherIcon.src = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
 };
 
 // get weather
